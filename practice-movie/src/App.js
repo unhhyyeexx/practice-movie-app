8c34ae5306a1e6e3cbc9c68b7fda51a68730a463
unhhyyeexx <unhhyyee66@gmail.com>
2022-07-07 22:18:06 +0900

    [todo]pre

---
+++ practice-movie/src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import {useState} from "react"
+
+function App() {
+  const [toDo, setToDo] = useState("")
+  const [toDos, setToDos] = useState([])
+  const onChange = (event) => setToDo(event.target.value)
+  const onSubmit = (event) => {
+    event.preventDefault()
+    if(toDo === ""){
+      return;
+    }
+    setToDos(currentArray => [toDo, ...currentArray])
+    setToDo("")
+  }
+  return (
+  <div>
+    <form onSubmit={onSubmit}>
+      <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..." />
+      <button>Add To Do</button>
+    </form>
+
+  </div>)
+}
+
+export default App;
