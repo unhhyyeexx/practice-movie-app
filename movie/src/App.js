16dd9cf01bb026781215bc56a7e9adf50f99f658
unhhyyeexx <unhhyyee66@gmail.com>
2022-07-10 01:09:00 +0900

    [movie_app] how to use router

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState, useEffect } from 'react';
-import Movie from "./components/Movie"
+// router는 URL을 보고있는 component
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom"
+import Home from "./routes/Home"
+import Detail from './routes/Detail';
+
 
 function App() {
-  return null
+  return <Router>
+    <Switch>
+      {/* switch가 하는 일: router를 찾는 일(url을 찾음) */}
+      <Route path="/movie">
+        <Detail/>
+      </Route>
+
+      <Route path="/">
+        <Home /> 
+      </Route>
+      
+
+    </Switch>
+  </Router>
 }
 
 export default App;
