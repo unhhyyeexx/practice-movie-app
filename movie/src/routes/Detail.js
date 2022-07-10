9263da45878a617ee3e36d7bfa47bc438909017f
unhhyyeexx <unhhyyee66@gmail.com>
2022-07-10 14:39:33 +0900

    [movie_app] simple detail page

--- movie/src/routes/Detail.js
+++ movie/src/routes/Detail.js
@@ -1,21 +1,42 @@
 import {useParams} from "react-router-dom"
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 
 function Detail() {
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([])
   const {id} = useParams()
 
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json()
-    console.log(json)
+    
+    setMovie(json.data.movie)
+    setLoading(false) 
   }
   
   useEffect(()=> {
     getMovie()
   }, [])
-  return <h1>Detail</h1>
+
+  return (
+    <div>
+      {loading ? <h1>loading</h1>
+      : <div>
+          <img src={movie.large_cover_image} alt={movie.title}></img>
+          <h2>{movie.title}</h2>
+          <p>realese year : {movie.year}</p>
+          <p>rating : {movie.rating}</p>
+          <p>runtime : {movie.runtime}</p>
+          <ul>
+            {movie.genres.map(genre => {
+              <li key={genre}>{genre}</li>
+            })}
+          </ul>
+        </div>}
+    </div>
+  )
 }
 
 export default Detail;
